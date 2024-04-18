61fa38016bc3d43bf247770ebeee7751e8a878f6
Ted Bonga <tedbonga4.tb@gmail.com>
2024-04-18 14:55:53 +0200

    Commit Files Project

---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Http\Controllers\AdminPostController;
+use App\Http\Controllers\NewsletterController;
+use App\Http\Controllers\PostCommentsController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionsController;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('ping', function(){
+//     $mailchimp = new \MailchimpMarketing\ApiClient();
+
+//     $mailchimp->setConfig([
+//         'apiKey'=>config('services.mailchimp.key'),
+//         'server'=>'us22'
+//     ]);
+
+//     $response=$mailchimp->ping->get();
+
+//     ddd($response);
+// });
+
+Route::post('newsletter',NewsletterController::class);
+
+
+Route::get('/', [PostController::class, 'index'])->name('home');
+
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
+Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
+
+Route::post('newsletter', NewsletterController::class);
+
+Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
+Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
+
+Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
+Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
+
+Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
+
+// Admin Section
+Route::middleware('can:admin')->group(function () {
+    Route::resource('admin/posts', AdminPostController::class)->except('show');
+});
